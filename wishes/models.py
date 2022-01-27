750ff8ffdff7ee63f11eed5a249a1527bdec2b7f
Timothy Odebunmi <dev.brymes@gmail.com>
2022-01-27 23:54:21 +0100

    migrations

--- wishes/models.py
+++ wishes/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Wishes(models.Model):
     
     id = models.AutoField(primary_key=True, unique=True)
-    name = models.CharField(max_length="30", default="Anonymous")
+    name = models.CharField(max_length=30, default="Anonymous")
     message = models.TextField(blank=False, null=True, help_text="Birthday Message")

fed36b5b0418774dbb961093f92dcdd217efa0a3
Timothy Odebunmi <dev.brymes@gmail.com>
2022-01-27 21:02:45 +0100

    Birthday Wish Model

--- wishes/models.py
+++ wishes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Wishes(models.Model):
+    
+    id = models.AutoField(primary_key=True, unique=True)
+    name = models.CharField(max_length="30", default="Anonymous")
+    message = models.TextField(blank=False, null=True, help_text="Birthday Message")

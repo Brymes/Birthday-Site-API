e27c7cb1ca5eed7e6241022b6d9880d613e7041c
Timothy Odebunmi <dev.brymes@gmail.com>
2022-01-27 23:47:54 +0100

    mutation for Birthday Completed

---
+++ gql/mutation.py
@@ -0,0 +1,27 @@
+from django.http import response
+import graphene
+from gql.types import CreateWishResponse
+
+from wishes.models import Wishes
+
+
+class AddWish(graphene.Mutation):
+
+    class Arguments:
+        name = graphene.String(required=True)
+        message = graphene.String(required=True)
+
+    add_wish_resp = graphene.Field(CreateWishResponse)
+
+    @classmethod
+    def mutate(cls, root, infom, name, message):
+
+        wish = Wishes(
+            name=name,
+            message=message
+        )
+
+        wish.save()
+
+        return cls(submit_response="True")
+

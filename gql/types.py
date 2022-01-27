9471de4dd04d84ed06028f11fa92fdfb9564428a
Timothy Odebunmi <dev.brymes@gmail.com>
2022-01-27 23:50:33 +0100

    custom response time

--- gql/types.py
+++ gql/types.py
@@ -1,3 +1,4 @@
+import graphene
 from graphene_django.types import DjangoObjectType
 
 from wishes.models import Wishes
@@ -7,3 +8,14 @@ class WishType(DjangoObjectType):
     class Meta:
         model = Wishes
         fields = "__all__"
+
+
+class CreateWishResponse(graphene.Scalar):
+
+    @staticmethod
+    def serialize(args_list):
+        if args_list:
+            response = {}
+
+            response["message"] = "Wish Submitted Successfully"
+            return response

d87d683d2e8d6475b024e737bd5db5047263058a
Timothy Odebunmi <dev.brymes@gmail.com>
2022-01-29 16:28:23 +0100

    Signal to Send Mails

---
+++ wishes/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.dispatch.dispatcher import receiver
+
+from .models import Wishes
+
+from django.core.mail import send_mail
+
+
+@receiver(post_save, sender=Wishes)
+def send_tribute_to_mail(sender, instance, created, **kwargs):
+    if created:
+        print(sender)
+
+        mess = f"{instance.message}"
+        subject = f"Birthday Tribute from {instance.name}"
+        send_mail(message=mess, from_email="", recepient_list=[
+                  "samuel.jennifer.olowo@gmail.com", "saolowo@yahoo.com"], subject=subject, fail_silently=False)

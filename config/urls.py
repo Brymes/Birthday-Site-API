fd0be585dca57105a1d8c60a6db37a3e7d767f16
Timothy Odebunmi <dev.brymes@gmail.com>
2022-01-29 08:36:30 +0100

    Trying another FIx for CORS errors

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,5 @@ from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',  csrf_exempt(GraphQLView.as_view(graphiql=False)))
+    path('api',  csrf_exempt(GraphQLView.as_view(graphiql=False)))
 ]

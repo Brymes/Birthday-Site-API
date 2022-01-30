18ca5dd711e046149441346d939de4c03820dd26
Timothy Odebunmi <dev.brymes@gmail.com>
2022-01-30 16:42:28 +0100

    Fixed from_email Bug

--- wishes/signals.py
+++ wishes/signals.py
@@ -1,8 +1,9 @@
 from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
 
-from .models import Wishes
+from django.conf import settings
 
+from .models import Wishes
 from django.core.mail import send_mail
 
 
@@ -12,7 +13,9 @@ def send_tribute_to_mail(sender, instance, created, **kwargs):
         print(sender)
         print("alknfasnalk")
 
+        from_email = settings.DEFAULT_FROM_EMAIL
+
         mess = f"{instance.message}"
         subject = f"Birthday Tribute from {instance.name}"
-        send_mail(message=mess, recipient_list=[
+        send_mail(message=mess, from_email=from_email, recipient_list=[
                   "samuel.jennifer.olowo@gmail.com", "saolowo@yahoo.com"], subject=subject, fail_silently=False)
